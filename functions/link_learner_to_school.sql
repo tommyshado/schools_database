f7e85444371e91cff3aac76c5a0b280125708800
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-05-02 09:39:21 +0200

    Modify query

--- functions/link_learner_to_school.sql
+++ functions/link_learner_to_school.sql
@@ -15,10 +15,10 @@ begin
 
     select into learner_count count(*) from learner where id = the_learner_id;
     select into school_count count(*) from school where id = the_school_id;
-    select into linked_learner count(*) from learner_school
-        where learner_school.learner_id = the_learner_id and learner_school.school_id = the_school_id;
 
     if (school_count = 1 and learner_count = 1) then
+        select into linked_learner count(*) from learner_school where learner_school.learner_id = the_learner_id;
+
         -- if a learner is not linked then
         if (linked_learner = 0) then
             -- retrieve the name from the school db then
