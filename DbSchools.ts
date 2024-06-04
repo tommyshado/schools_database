faf912c5ef9f69cf4fd0d3db899817eede20f2f4
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-06-04 22:06:12 +0200

    Handling errors @DbTeachers.ts & @DbSchools.ts

--- DbSchools.ts
+++ DbSchools.ts
@@ -6,6 +6,7 @@ export default class DbSchools implements DbSchoolsInt {
 
     async createSchools(name: string, region: string): Promise<boolean> {
         try {
+            if (!(name && region)) return false;
             const query = "select * from create_schools($1, $2)";
             const results = await this.db.oneOrNone(query, [name, region]);    
             return results.create_schools;
